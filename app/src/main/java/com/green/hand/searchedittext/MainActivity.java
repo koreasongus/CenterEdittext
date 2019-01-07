7b659c51aa45e72ee364b578e80c25cf6423dd2c
OneGreenHand <295268305@qq.com>
2019-01-07 15:47:44 +0800

    第一次提交

---
+++ app/src/main/java/com/green/hand/searchedittext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.green.hand.searchedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
